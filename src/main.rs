409aa9762a101c251c15623ac14302d193dd9e85
Tarnished <susan.dev291@gmail.com>
2026-06-02 09:01:46 +0545

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,61 @@
+use std::env;
+use std::fs;
+use std::fs::read_dir;
+
+fn main() {
+    // Getting the path
+    let args: Vec<String> = env::args().collect();
+    let path = match args.get(1) {
+        Some(p) => p,
+        None => {
+            println!("Please Provide a folder path to clean!");
+            return;
+        }
+    };
+
+    // Validating the path
+    let files = match read_dir(path) {
+        Ok(files) => files,
+        Err(e) => {
+            println!("Error: {e}");
+            return;
+        }
+    };
+
+    let output_base = "D:/rustBroom_test/output";
+    // Itertaing path item
+    for i in files {
+        match i {
+            Ok(entry) => {
+                let path = entry.path();
+                let name = entry.file_name().to_string_lossy().into_owned();
+                let extension = path.extension().and_then(|e| e.to_str());
+
+                let category = match extension {
+                    Some("rs") => "code",
+                    Some("pdf") | Some("txt") | Some("csv") => "documents",
+                    Some("json") => "Sheet",
+                    Some("zip") => "Zip",
+                    Some("msi") | Some("exe") => "Installer",
+                    Some("toml") | Some("lock") => "build",
+                    Some("png") | Some("jpg") => "images",
+                    Some("mp4") => "videos",
+                    Some(other) => other,
+                    None => "folder/unknown",
+                };
+
+                let target_folder = format!("{output_base}/{category}");
+                if let Err(e) = fs::create_dir_all(&target_folder) {
+                    println!("Failed to create folder: {}", e);
+                    continue;
+                }
+
+                println!("File name {name}, Category: {:?}", category);
+            }
+            Err(e) => {
+                println!("Error: {e}");
+                return;
+            }
+        }
+    }
+}
